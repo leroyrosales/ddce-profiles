42a512a01719aadbe2bb7f9ff8608cf3f8e05c87
Leroy Rosales <leroyrosales@gmail.com>
2021-06-21 14:50:23 -0500

    Init CPT setup

--- ddce-faculty-profiles.php
+++ ddce-faculty-profiles.php
@@ -14,10 +14,47 @@ Class DiversityFacultyProfiles {
 
   private static $instance = null;
 
+  public function __construct() {
+
+    add_action( 'init', [$this, 'initialize'], 0, 0 );
+
+  }
+
   public static function instance() {
     if( isset( self::$instance) ) return self::$instance;
 
     return self::$instance = new DiversityFacultyProfiles();
+  }
+
+  public function initialize() {
+    // Bail early if called directly from functions.php or plugin file.
+    if( !did_action( 'plugins_loaded' ) ) return;
+
+    $this->register_profiles_cpt();
+
+    register_deactivation_hook( __FILE__, [$this, 'deactivate'] );
+  }
+
+  // Register Faculty Profiles post type
+  private function register_profiles_cpt() {
+    register_post_type( 'ddce-faculty-profile', [
+      'labels' => [
+        'name' => __( 'Faculty Profiles', 'ddce-faculty-profile' ),
+        'singular_name' => __( 'Faculty Profile', 'ddce-faculty-profile' ),
+      ],
+      'public' => true,
+      'has_archive' => false,
+      'menu_icon' => 'dashicons-businessperson',
+      'publicly_queryable' => true,
+      'rewrite' => true,
+      'show_ui' => true,
+      'supports' => [
+        'title',
+        'thumbnail',
+        'custom-fields',
+      ],
+    ]);
+
   }
 
 } // End DiversityFacultyProfiles class
